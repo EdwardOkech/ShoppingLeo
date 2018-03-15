01730938a4609e5958462c5c04147298dc3e4a27
EdwardOkech <okechjobs@gmail.com>
2018-03-15 14:38:41 +0300

    minor change

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,17 +1,33 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import firebase from 'firebase';
-/*
-  Generated class for the AuthProvider provider.
+import { User } from '@firebase/auth-types';
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class AuthProvider {
+  constructor () {}
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AuthProvider Provider');
+  loginUser(email: string, password: string): Promise<User>{
+    return firebase.auth().signInWithEmailAndPassword(email, password);
   }
 
+  signupUser(email: string, password: string): Promise<any> {
+    return firebase.auth().createUserWithEmailAndPassword(email, password).then(newUser => {
+      firebase.database().ref(`/userProfile/${newUser.uid}/email`).set(email);
+    })
+    .catch(error => {
+      console.error(error);
+      throw new Error(error); 
+    }); 
+  }
+
+  resetPassword(email:string): Promise<void> {
+    return firebase.auth().sendPasswordResetEmail(email);
+  }
+  logoutUser(): Promise<void>{
+    const userId : string = firebase.auth().currentUser.uid;
+    firebase.database().ref(`/userProfile/${userId}`).off();
+    return firebase.auth().signOut();
+  }
+  
 }
+
