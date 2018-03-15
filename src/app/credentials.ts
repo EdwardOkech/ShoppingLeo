ccbb3aa5e41ddbc3a518b55b63711d4d8b561d37
Edward Okech <okechjobs@gmail.com>
2018-03-15 14:43:56 +0300

    delete this unneccesary file

--- src/app/credentials.ts
+++
@@ -1,9 +0,0 @@
-// Intitialize Firebase
-export const firebaseConfig = {
-    apiKey: "AIzaSyBpAfopzkTnydFDvn3st4iW37yZbRz2VQQ",
-    authDomain: "shop-ryt.firebaseapp.com",
-    databaseURL: "https://shop-ryt.firebaseio.com",
-    projectId: "shop-ryt",
-    storageBucket: "",
-    messagingSenderId: "970123747821"
-};
